8e445f967034c26c5ae986bdaa291a9e10444ec6
SAMBEET SEKHAR PANDA <sambeetpanda507@gmail.com>
2022-08-19 22:40:24 +0530

    added robot in landing page

--- components/Home/LandingHeroImage.tsx
+++ components/Home/LandingHeroImage.tsx
@@ -1,5 +1,13 @@
 import { FC } from 'react'
 import { motion } from 'framer-motion'
+import dynamic from 'next/dynamic'
+
+const DynamicDrone = dynamic(() => import('./Drone'), {
+  ssr: false,
+  loading: () => {
+    return <div>loading...</div>
+  },
+})
 
 export const LandingHeroImage: FC = () => {
   return (
@@ -8,18 +16,7 @@ export const LandingHeroImage: FC = () => {
       animate={{ opacity: 1, y: 0 }}
       className="h-96 relative md:col-start-2 md:row-start-1 mt-4 md:mt-0"
     >
-      {/* circle 1 */}
-      <div className="w-72 md:w-96 aspect-square bg-gray-100 absolute rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-      {/* circle 2 */}
-      <div className="w-60 md:w-80 aspect-square bg-gray-200 absolute rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-      {/* circle 3 */}
-      <div className="w-48 md:w-64 aspect-square bg-gray-300 absolute rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <img
-          src="https://picsum.photos/1000/1000?random=1"
-          alt="picture"
-          className="w-full aspect-square rounded-full"
-        />
-      </div>
+      <DynamicDrone />
     </motion.div>
   )
 }
