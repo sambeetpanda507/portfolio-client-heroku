d361f739da41260c0c2e3a1401d6bcbea49b1187
SAMBEET SEKHAR PANDA <sambeetpanda507@gmail.com>
2022-08-20 11:14:07 +0530

    added drone video

--- components/Home/LandingHeroImage.tsx
+++ components/Home/LandingHeroImage.tsx
@@ -1,22 +1,22 @@
 import { FC } from 'react'
 import { motion } from 'framer-motion'
-import dynamic from 'next/dynamic'
-
-const DynamicDrone = dynamic(() => import('./Drone'), {
-  ssr: false,
-  loading: () => {
-    return <div>loading...</div>
-  },
-})
 
 export const LandingHeroImage: FC = () => {
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
-      className="h-96 relative md:col-start-2 md:row-start-1 mt-4 md:mt-0"
+      className="h-96 relative md:col-start-2 md:row-start-1 mt-4 md:mt-0 overflow-hidden"
     >
-      <DynamicDrone />
+      <video
+        autoPlay
+        muted
+        playsInline
+        loop
+        className="w-full h-full scale-[1.65]"
+      >
+        <source src="/drone.webm" type="video/webm" />
+      </video>
     </motion.div>
   )
 }
